1f57b7c0af5957820c9da2ff48a519b5854fab7e
Saurabh Singh <68167144+iam-saurabhsingh@users.noreply.github.com>
2024-07-10 17:04:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+#resource to create a gcp bucket
+resource "google_storage_bucket" "my-bucket1" {
+  name                     = "tt-githubdemo-bucket-001"
+  project                  = "tt-dev-001"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+#
+resource "google_storage_bucket" "my-bucket2" {
+  name                     = "tt-githubdemo-bucket-002"
+  project                  = "tt-dev-001"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
