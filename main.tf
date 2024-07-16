57294474d7f040419b2b97d066e42504cc72b4c8
Saurabh Singh <68167144+iam-saurabhsingh@users.noreply.github.com>
2024-07-16 12:07:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "google_storage_bucket" "my-bucket2" {
   public_access_prevention = "enforced"
 }
 
-provider "google" {
-  access_token = "ya29.a0AXooCgvS9ecdDSTNokS2Xj-RGAAk01s5pEo66pVJQW69Y9yjgIn3iw60YrHx6neeDx-OmKZLc8jQE0MApVtTqdNqp7yyuY8NeNpqkHobeSCI08KfGQ4pgpMUdksVtlVLxjukh7MQDYC162c0MRTXWBQeGu_PsYiQuizoVPm_77SC36XHem5hwyw8vYLTchUa86Ci1vYQ9Avjbk_YTsnMJlG1UlHfsEzRH9RjK_AZr0SSCIPXswUsI_wSmyF_8sAo97Dfxk3hN6n37FGo63qk8MAukoqtudPCCplKLO0BDVj0E7GVWiGT3eb4cEgkoYmMYZdiZjy84s5yHRN5ZJrYbOZzpwfDjRTjySHoxAScNgYQJY-cMW2JhopgcJUw-sPQ9YBvjCQ6PAYyEP4OX81jDRkLOZwgKxsaCgYKAawSARASFQHGX2MiSGGMW93NEr8-PpxgVleLeQ0422"
+#provider "google" {
+  #access_token = ""
 
-}
+#}
